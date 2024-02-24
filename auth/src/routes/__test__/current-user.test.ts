8b0a1e76b91aefc839f0483f0cd04b6c2d430869
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-24 14:06:58 +0200

    signin func in setup

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,14 +2,7 @@ import request from 'supertest';
 import {app} from '../../app';
 
 it('responds with details about the current user', async () => {
-    const authResponse = await request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(201);
-    const cookie = authResponse.get('Set-Cookie');
+    const cookie = await global.signin();
 
     const response = await request(app)
         .get('/api/users/currentuser')
@@ -18,4 +11,13 @@ it('responds with details about the current user', async () => {
         .expect(200);
 
     expect(response.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responds with null if not authenticated', async () => {
+    const response = await request(app)
+        .get('/api/users/currentuser')
+        .send()
+        .expect(200);
+
+    expect(response.body.currentUser).toEqual(null);
 });
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,10 +1,17 @@
 import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import {app} from '../app';
+
+declare global {
+    var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
-    process.env.JWT_KEY = "Some"
+    process.env.JWT_KEY = 'asdfasdf';
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
     const mongo = await MongoMemoryServer.create();
     const mongoUri = mongo.getUri();
 
@@ -25,3 +32,20 @@ afterAll(async () => {
     }
     await mongoose.connection.close();
 });
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email,
+            password,
+        })
+        .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+};
