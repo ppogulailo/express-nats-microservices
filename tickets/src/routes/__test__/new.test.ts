89376b74ad962bf520e048a62638aebdb8cdb5df
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-24 16:54:45 +0200

    validation, and test for it

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -12,15 +12,50 @@ it('can only be accessed if the user is signed in', async () => {
 });
 
 it('returns a status other than 401 if the user is signed in', async () => {
-    const response = await request(app).post('/api/tickets').send({});
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({});
 
     expect(response.status).not.toEqual(401);
 });
 
 it('returns an error if an invalid title is provided', async () => {
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: '',
+            price: 10,
+        })
+        .expect(400);
+
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            price: 10,
+        })
+        .expect(400);
 });
 
 it('returns an error if an invalid price is provided', async () => {
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'asldkjf',
+            price: -10,
+        })
+        .expect(400);
+
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'laskdfj',
+        })
+        .expect(400);
 });
 
 it('creates a ticket with valid inputs', async () => {
