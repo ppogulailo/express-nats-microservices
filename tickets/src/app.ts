3321181d4ba0dc77caf2bf05ca165e08d040cfc9
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-24 16:52:01 +0200

    signin func change

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,8 @@ import {json} from "body-parser";
 import 'express-async-errors'
 import cookieSession from "cookie-session";
 
-import {errorHandler, NotFoundError} from "@pogmicro/common";
+import {errorHandler, NotFoundError, currentUser} from "@pogmicro/common";
+import {createTicketRouter} from "./routes/__test__/new";
 
 const app = express()
 app.set('trust proxy', true)
@@ -11,8 +12,9 @@ app.set('trust proxy', true)
 
 app.use(json());
 app.use(cookieSession({signed: false, secure: process.env.NODE_ENV !== 'test'}))
+app.use(currentUser)
 
-
+app.use(createTicketRouter)
 app.all('*', async () => {
     throw new NotFoundError()
 })
