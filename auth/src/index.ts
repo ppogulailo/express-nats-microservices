31166de4a31606d25c2265a85efca3bbc560ca07
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-24 16:27:22 +0200

    mongo uri as env var insive infra

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,7 +6,7 @@ const start = async () => {
         throw new Error('JWT_KEY must be defined!')
     }
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/')
+        await mongoose.connect(process.env.MONGO_URI!)
         console.log('Connected to MongoDB')
     } catch (e) {
         console.error(e)
