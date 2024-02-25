d0accc7c87a4355cb6cebe1d7ce35eadf5249120
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-25 19:55:54 +0200

    SEC-17 orders new

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,6 +3,10 @@ import 'express-async-errors';
 import {json} from 'body-parser';
 import cookieSession from 'cookie-session';
 import {errorHandler, NotFoundError, currentUser} from '@rallycoding/common';
+import {deleteOrderRouter} from './routes/delete';
+import {indexOrderRouter} from './routes/index';
+import {newOrderRouter} from './routes/new';
+import {showOrderRouter} from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,6 +19,10 @@ app.use(
 );
 app.use(currentUser);
 
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
