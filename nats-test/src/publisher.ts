7746a0f8c5b422fb644799bd3767ae8e9399fe8c
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-25 15:55:53 +0200

    publisher abstract

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,6 @@
 import nats from 'node-nats-streaming';
+import {TicketCreatedPublisher} from "./events/ticket-created-publisher";
+import {Subjects} from "./events/subjects";
 
 console.clear();
 
@@ -6,16 +8,16 @@ const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS');
-
-    const data = JSON.stringify({
-        id: '123',
-        title: 'concert',
-        price: 20,
-    });
-
-    stan.publish('ticket:created', data, () => {
-        console.log('Event published');
-    });
+    const publisher = new TicketCreatedPublisher(stan)
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 20,
+        })
+    } catch (e) {
+        console.log(e)
+    }
 });
