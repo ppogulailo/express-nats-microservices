47cd9470f628d81ad3c056a03ac378b89abf537e
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-25 17:40:06 +0200

    ingress update

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,24 +1,33 @@
-import mongoose from "mongoose";
-import {app} from "./app";
-import {natsWrapper} from "./nats-wrapper";
+import mongoose from 'mongoose';
+
+import {app} from './app';
+import {natsWrapper} from './nats-wrapper';
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY must be defined!')
+        throw new Error('JWT_KEY must be defined');
     }
     if (!process.env.MONGO_URI) {
-        throw new Error('MONGO_URI must be defined!')
+        throw new Error('MONGO_URI must be defined');
     }
-    try {
-        await natsWrapper.connect('ticketing', 'randomString', 'http://nats-srv:4222')
-        await mongoose.connect(process.env.MONGO_URI!)
-        console.log('Connected to MongoDB')
-    } catch (e) {
-        console.error(e)
-    }
-    app.listen(3001, () => {
-        console.log('Listening on 3001')
-    })
-}
 
-start()
+    try {
+        await natsWrapper.connect('ticketing', 'alsdkj', 'http://nats-srv:4222');
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed!');
+            process.exit();
+        });
+        process.on('SIGINT', () => natsWrapper.client.close());
+        process.on('SIGTERM', () => natsWrapper.client.close());
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MongoDb');
+    } catch (err) {
+        console.error(err);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!!!!!!!');
+    });
+};
+
+start();
