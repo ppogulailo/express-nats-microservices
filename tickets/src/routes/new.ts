060d2ae8b213c08db2d2e226f5336d2211877460
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-25 16:58:12 +0200

    natsWrapper singleton

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from 'express';
 import {body} from 'express-validator';
 import {requireAuth, validateRequest} from '@pogmicro/common';
 import {Ticket} from "../models/ticket";
+import {TicketCreatedPublisher} from "../events/publishers/ticket-created-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -23,6 +25,12 @@ router.post(
             price,
             userId: req.currentUser!.id,
         });
+        new TicketCreatedPublisher(natsWrapper.client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId
+        })
         await ticket.save();
 
         res.status(201).send(ticket);
