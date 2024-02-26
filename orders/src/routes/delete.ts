be46709c9166670cf95dab6bdb310f19ee06cc0b
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-26 11:15:07 +0200

    SEC-18 tests for new and delete routes

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,6 +5,8 @@ import {
     NotAuthorizedError,
 } from '@rallycoding/common';
 import {Order, OrderStatus} from '../models/order';
+import {OrderCancelledPublisher} from "../events/publisher/order-cancelled-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -14,7 +16,7 @@ router.delete(
     async (req: Request, res: Response) => {
         const {orderId} = req.params;
 
-        const order = await Order.findById(orderId);
+        const order = await Order.findById(orderId).populate('ticket');
 
         if (!order) {
             throw new NotFoundError();
@@ -26,6 +28,13 @@ router.delete(
         await order.save();
 
         // publishing an event saying this was cancelled!
+        new OrderCancelledPublisher(natsWrapper.client).publish({
+            id: order.id,
+            ticket: {
+                id: order.ticket.id
+            }
+        })
+
 
         res.status(204).send(order);
     }
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -10,6 +10,8 @@ import {
 import {body} from 'express-validator';
 import {Ticket} from '../models/ticket';
 import {Order} from '../models/order';
+import {OrderCreatedPublisher} from "../events/publisher/order-created-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -55,6 +57,17 @@ router.post(
         await order.save();
 
         // Publish an event saying that an order was created
+        new OrderCreatedPublisher(natsWrapper.client).publish({
+            id: order.id,
+            status: order.status,
+            userId: order.userId,
+            expiresAt: order.expiresAt.toISOString(),
+            ticket: {
+                id: ticket.id,
+                price: ticket.price
+            }
+
+        })
 
         res.status(201).send(order);
     }
