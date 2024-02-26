8a2146526f983235ffe212a36277597d57d797c8
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-26 14:49:36 +0200

    SEC-20 bull-queue

--- expiration/src/nats-wrapper.ts
+++ expiration/src/nats-wrapper.ts
@@ -1,37 +1,29 @@
-import nats, {Stan} from 'node-nats-streaming';
+import nats, { Stan } from 'node-nats-streaming';
 
 class NatsWrapper {
-    private _client?: Stan;
+  private _client?: Stan;
 
-    get client() {
-        if (!this._client) {
-            throw new Error('Cannot access NATS client before connecting');
-        }
-
-        return this._client;
+  get client() {
+    if (!this._client) {
+      throw new Error('Cannot access NATS client before connecting');
     }
 
-    connect(clusterId: string, clientId: string, url: string) {
-        this._client = nats.connect(clusterId, clientId, {url});
+    return this._client;
+  }
 
-        return new Promise<void>((resolve, reject) => {
-            this.client.on('connect', () => {
-                console.log('Connected to NATS');
-                resolve();
-            });
-            this.client.on('error', (err) => {
-                reject(err);
-            });
-        });
-    }
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise<void>((resolve, reject) => {
+      this.client.on('connect', () => {
+        console.log('Connected to NATS');
+        resolve();
+      });
+      this.client.on('error', (err) => {
+        reject(err);
+      });
+    });
+  }
 }
 
 export const natsWrapper = new NatsWrapper();
-// - image: YOUR_USERNAME/expiration
-// context: expiration
-// docker:
-//     dockerfile: Dockerfile
-// sync:
-//     manual:
-//         - src: 'src/**/*.ts'
-// dest: .
