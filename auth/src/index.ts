48ca2f4cf4f3510b4c57cdf25ada5561e0461c5f
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-27 13:53:58 +0200

    auth start log

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import {app} from "./app";
 
 const start = async () => {
+    console.log('Starting auth service ...')
+
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined!')
     }
