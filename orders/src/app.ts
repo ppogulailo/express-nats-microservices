f8532252c7b64941b2927ac19513b002bf3dd03b
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-27 10:43:59 +0200

    SEC-20 oreders fix

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -1,21 +1,21 @@
 import express from 'express';
 import 'express-async-errors';
-import {json} from 'body-parser';
+import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import {errorHandler, NotFoundError, currentUser} from '@rallycoding/common';
-import {deleteOrderRouter} from './routes/delete';
-import {indexOrderRouter} from './routes/index';
-import {newOrderRouter} from './routes/new';
-import {showOrderRouter} from './routes/show';
+import { errorHandler, NotFoundError, currentUser } from '@rallycoding/common';
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
 app.use(
-    cookieSession({
-        signed: false,
-        secure: process.env.NODE_ENV !== 'test',
-    })
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
 );
 app.use(currentUser);
 
@@ -25,9 +25,9 @@ app.use(newOrderRouter);
 app.use(showOrderRouter);
 
 app.all('*', async (req, res) => {
-    throw new NotFoundError();
+  throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
-export {app};
+export { app };
