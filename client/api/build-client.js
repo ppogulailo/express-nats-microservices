76d44caf74080174692c307d5f49685189a738d9
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-27 15:54:38 +0200

    build client change to http

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({req}) => {
         // We are on the server
 
         return axios.create({
-            baseURL: 'www.stormerify.com',
+            baseURL: 'http://www.stormerify.com/',
             headers: req.headers,
         });
     } else {
