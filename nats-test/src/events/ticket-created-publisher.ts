1eaee39c267cb3f343a21983da1e73610074e537
Pavel Pogulailo <pavel.pogulailo@gmail.com>
2024-03-06 15:21:22 +0200

    Delete nats-test directory

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,8 +0,0 @@
-import {Publisher} from "./base-publisher";
-import {Subjects} from "./subjects";
-import {TicketCreatedEvent} from "./ticket-created-event";
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-    subject: Subjects.TicketCreated = Subjects.TicketCreated;
-
-}
